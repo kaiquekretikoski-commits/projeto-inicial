47269420f208792638b3cab538d08831a2028ab4
kaiquekretikoski-commits <kaique.kretikoski@escola.pr.gov.br>
2026-03-12 10:39:48 -0300

    alterar quantidade de numero

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 10 e 100');
+    exibirTextoNaTela('p', 'Escolha um número entre 100 e 100');
 }
 
 exibirMensagemInicial();
